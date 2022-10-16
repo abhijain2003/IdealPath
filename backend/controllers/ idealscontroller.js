886056e454a9bc5ae3cf706094165a3f84be699b
Palash Baderia <palash.baderia@outlook.com>
2022-10-16 20:06:58 +0530

    added podcats

--- backend/controllers/ idealscontroller.js
+++ backend/controllers/ idealscontroller.js
@@ -3,7 +3,7 @@ const ideal = require("../models/data.json");
 exports.idealinfo = (req, res) => {
   try {
     var data = JSON.stringify(ideal);
-    res.status(201).send(data);
+    res.status(201).send({"data":data})
   } catch (err) {
     res.status(401).json({
       message: "no User Found",
